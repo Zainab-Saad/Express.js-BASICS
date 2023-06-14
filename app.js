b8b471cb90d438ef3618e1e2bd745ea46b6097d8
Zainab Saad <zsaad.bese21seecs@seecs.edu.pk>
2023-06-14 12:13:11 +0500

    Add and serve html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const path = require('path');
 
 const express = require("express");
 const bodyParser = require('body-parser');
@@ -11,12 +12,16 @@ const app = express(); // initialize an express app
 // --- defining middlewares, receiving, parsing requests and redirecting responses
 // parse the req body
 app.use(bodyParser.urlencoded({extended : false}));
+
+
+// add static middleware
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes); // router is a valid middleware
 app.use(shopRoutes);
 
 // send 404 error page - catch all route
 app.use('/', (req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 const server = http.createServer(app); // create a server and give a valid request handler (app in this case)
